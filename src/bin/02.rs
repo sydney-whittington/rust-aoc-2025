61dbe18bd6216a8c43a32999ddcdb4fcff0af4b8
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2025-12-02 15:24:15 -0500

    day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use nom::{
     IResult, Parser, bytes::complete::tag, character::complete::u128, multi::separated_list1,
     sequence::separated_pair,
@@ -12,9 +13,7 @@ fn parser(i: &str) -> IResult<&str, Vec<(u128, u128)>> {
 pub fn part_one(input: &str) -> Option<u128> {
     let (_, pairs) = parser(input).unwrap();
 
-    println!("{:?}", pairs);
-
-    let mut bad = vec![];
+    let mut bad = 0;
 
     for pair in pairs.iter() {
         for n in pair.0..=pair.1 {
@@ -22,16 +21,38 @@ pub fn part_one(input: &str) -> Option<u128> {
             // differences in half lengths for odd ones just mean it won't ever be doubled
             let doubled = id[..id.len() / 2] == id[id.len() / 2..];
             if doubled {
-                bad.push(n);
+                bad += n;
             }
         }
     }
 
-    Some(bad.iter().sum())
+    Some(bad)
 }
 
-pub fn part_two(_input: &str) -> Option<u128> {
-    None
+pub fn part_two(input: &str) -> Option<u128> {
+    let (_, pairs) = parser(input).unwrap();
+
+    let mut bad = 0;
+
+    for pair in pairs.iter() {
+        for n in pair.0..=pair.1 {
+            let id = n.to_string();
+            let mut doubled = false;
+            for length in 1..=id.len() / 2 {
+                doubled |= id
+                    .chars()
+                    .chunks(length)
+                    .into_iter()
+                    .map(|c| c.collect::<String>())
+                    .all_equal();
+            }
+            if doubled {
+                bad += n;
+            }
+        }
+    }
+
+    Some(bad)
 }
 
 #[cfg(test)]
@@ -47,6 +68,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
