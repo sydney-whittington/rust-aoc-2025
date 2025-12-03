7fe6c9565ba8e92f947e95475151d1a8ee1af631
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2025-12-03 10:05:31 -0500

    day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,11 +1,11 @@
-use itertools::{Itertools};
+use iter_first_max::IterFirstMaxExt as _;
+use itertools::Itertools;
 use nom::{
     IResult, Parser,
     character::complete::{digit1, newline},
     combinator::map,
     multi::separated_list1,
 };
-use iter_first_max::IterFirstMaxExt as _;
 
 advent_of_code::solution!(3);
 
@@ -29,22 +29,44 @@ pub fn part_one(input: &str) -> Option<u64> {
     for battery in batteries.iter() {
         let (first_max_index, first_max_value) = battery
             .iter()
-            .dropping_back(1) // can't have the max be last or there isn't another digit after
             .enumerate()
+            .dropping_back(1) // can't have the max be last or there isn't another digit after
             .first_max_by_key(|(_index, joltage)| **joltage)
             .unwrap();
 
         let second_max_value = battery.iter().skip(first_max_index + 1).max().unwrap();
 
-        println!("for battery {:?}, first max is {}, second max is {}", battery.iter().join(""), first_max_value, second_max_value);
         total += (first_max_value * 10 + second_max_value) as u64;
     }
 
-    Some(total as u64)
+    Some(total)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u128> {
+    let (_, batteries) = parser(input).unwrap();
+
+    let mut total: u128 = 0;
+
+    for battery in batteries.iter() {
+        let mut battery_total: u128 = 0;
+        let mut split_index = 0;
+        for i in (0..12).rev() {
+            let (current_max_index, current_max_value) = battery
+                .iter()
+                .enumerate()
+                .dropping_back(i) // can't have the max be closer to the end than the number of remaining digits
+                .skip(split_index)
+                .first_max_by_key(|(_index, joltage)| **joltage)
+                .unwrap();
+
+            split_index = current_max_index + 1;
+            battery_total = battery_total * 10 + *current_max_value as u128;
+        }
+
+        total += battery_total;
+    }
+
+    Some(total)
 }
 
 #[cfg(test)]
@@ -60,6 +82,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
