8db5f6e9f39aea84082b8e7dd400f3e54b3d9dd3
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2025-12-06 08:46:52 -0500

    day 5 part 2 (after many off by one errors)

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -2,7 +2,10 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 use nom::{
-    IResult, Parser, bytes::complete::tag, character::complete::{newline, u128}, multi::separated_list1,
+    IResult, Parser,
+    bytes::complete::tag,
+    character::complete::{newline, u128},
+    multi::separated_list1,
     sequence::separated_pair,
 };
 
@@ -26,11 +29,53 @@ pub fn part_one(input: &str) -> Option<u128> {
         }
     }
 
-    Some(fresh as u128)
+    Some(fresh)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u128> {
+    let (_, (ranges, _)) = parser(input).unwrap();
+    let ranges = ranges.into_iter().sorted_by_key(|(l, _)| *l).collect_vec();
+
+    let mut valids: HashSet<(u128, u128)> = HashSet::new();
+    for (lower, upper) in ranges.iter() {
+        if valids.iter().any(|(l, u)| *l <= *lower && *u >= *upper) {
+            // introduces no new range, ignore
+            continue;
+        } else if let Some(mut subsumed) = valids
+            .iter()
+            .find(|(l, u)| *l <= *lower && *lower <= *u && *u <= *upper)
+            .cloned()
+        {
+            // lower is between and upper is above (or equal)
+            valids.remove(&subsumed);
+            subsumed.1 = *upper;
+            valids.insert(subsumed);
+        } else if let Some(mut subsumed) = valids
+            .iter()
+            .find(|(l, u)| *l >= *lower && *l <= *upper && *u >= *upper)
+            .cloned()
+        {
+            // upper is between and lower is below (or equal)
+            valids.remove(&subsumed);
+            subsumed.0 = *lower;
+            valids.insert(subsumed);
+        } else {
+            // remove anything entirely contained
+            for valid in valids
+                .iter()
+                .filter(|(l, u)| *l >= *lower && *u <= *upper)
+                .cloned()
+                .collect::<Vec<_>>()
+            {
+                valids.remove(&valid);
+            }
+            valids.insert((*lower, *upper));
+        }
+    }
+
+    let fresh = valids.iter().map(|(l, u)| u - l + 1).sum();
+
+    Some(fresh)
 }
 
 #[cfg(test)]
@@ -46,6 +91,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
     }
 }
